67d3b172b412abc17c589373d60bdd922feb8e01
unknown <prabesh037@gmail.com>
2022-12-14 19:30:28 +0545

    changes

--- src/Component/header/Social.js
+++ src/Component/header/Social.js
@@ -5,7 +5,7 @@ import {BsInstagram} from 'react-icons/bs'
 function Social() {
   return (
     <div className='header__social'>
-        <a href="https://www.facebook.com/prabesh.khanal.7906" target="_blank"  rel="noopener noreferrer"><BsFacebook/></a>
+      <a href="https://www.facebook.com/prabesh.khanal.7906" target="_blank" rel="noopener noreferrer"><BsFacebook/></a>
         <a href="https://github.com/Prabesh69" target="_blank"  rel="noopener noreferrer"><BsGithub/></a>
         <a href="https://www.instagram.com/entry069/" target="_blank"  rel="noopener noreferrer"><BsInstagram/></a>
     </div>
