1381349a403f51f7daf66c4c19d6af5b4bb98ef5
unknown <prabesh037@gmail.com>
2022-12-16 08:42:38 +0545

    changes

--- src/Component/header/CTA.js
+++ src/Component/header/CTA.js
@@ -6,17 +6,12 @@ function CTA() {
     const color = getComputedStyle(document.documentElement).getPropertyValue('--color-primary')
     console.log(color)
   },[])
-function changetheme(color ){
-  document.documentElement.style.setProperty('--color-primary', )
-}
-function changetheme(){
-  
-}
+
 
   return (
     <div className='cta'>
         {/* <a href={CV} className="btn">Download CV</a> */}
-        <button className="btn theme" onClick={()=>{changetheme(active)}}>Change-Theme</button>
+        <button className="btn theme" >Change-Theme</button>
 
         <a href='#contact' className='btn btn-primary'>Let's Talk</a>
 
