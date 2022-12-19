c994d799b41e8702ad614e612111cdd3466e0cff
unknown <prabesh037@gmail.com>
2022-12-19 16:43:24 +0545

    changes

--- src/Component/header/CTA.js
+++ src/Component/header/CTA.js
@@ -22,7 +22,7 @@ function setcolor(){
   return (
     <div className='cta'>
         {/* <a href={CV} className="btn">Download CV</a> */}
-        <button className="btn theme" onClick={ setcolor} >Change-Theme</button>
+        <button className="btn theme" onClick={ setcolor} >Change Theme</button>
 
         <a href='#contact' className='btn btn-primary'  rel="noopener noreferrer">Let's Talk</a>
 
