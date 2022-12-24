b6ba0f3065570afc01c9504c1cb5462fa3c188e5
unknown <prabesh037@gmail.com>
2022-12-24 08:55:56 +0545

    changes

--- src/Component/header/CTA.js
+++ src/Component/header/CTA.js
@@ -7,6 +7,8 @@ function setcolor(){
     document.documentElement.style.setProperty('--color-primary', '#34ff71')
     document.documentElement.style.setProperty("--color-primary-variant",'  rgba(77, 255, 157, 0.4)')
     document.documentElement.style.setProperty("--color-bg-variant",'  #30861b')
+    document.documentElement.style.setProperty("--color-hover","#4db5ff")
+
 
 
 
@@ -14,6 +16,8 @@ function setcolor(){
     document.documentElement.style.setProperty('--color-primary', "#4db5ff")
     document.documentElement.style.setProperty("--color-primary-variant",'rgba(77,181,255,0.4)  ')
     document.documentElement.style.setProperty("--color-bg-variant",'#2c2c6c')
+    document.documentElement.style.setProperty("--color-hover",'#34ff71')
+    
 
   }
   console.log("button clicked")
