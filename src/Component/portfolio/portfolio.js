50d49fd0f2cece6f0be8953b72070043bc75b3ea
Prabesh69 <prabesh037@gmail.com>
2024-06-04 19:19:31 +0545

    changes

--- src/Component/portfolio/portfolio.js
+++ src/Component/portfolio/portfolio.js
@@ -1,61 +1,90 @@
-import React from 'react'
-import './portfolio.css'
-import RE from '../../assets/portfolio1.jpg'
+import React from 'react';
+import './portfolio.css';
+import RE from '../../assets/portfolio1.jpg';
+import JS from '../../assets/JS.jpeg';
 
-import ME from '../../assets/skynewz.png'
-import {BsGithub} from 'react-icons/bs'
-
-
+import ME from '../../assets/skynewz.png';
+import { BsGithub } from 'react-icons/bs';
 
 function Portfolio() {
   return (
-<section id='portfolio'>
-  <h5>My Recent Works .</h5>
-  <h2>Portfolio</h2>
-  <div className="container portfolio_container">
-{/* Fir */}
+    <section id="portfolio">
+      <h5>My Recent Works</h5>
+      <h2>Portfolio</h2>
+      <div className="container portfolio_container">
+        {/* First */}
+        <article className="portfolio_item">
+          <a href="https://sky-newz.netlify.app/" target="_blank" rel="noreferrer">
+            <div className="hold">
+              <div className="portfolio_item-image">
+                <img src={ME} alt="Project" />
+              </div>
+              <div className="information">
+                <h3 className="h3_prot">Sky-Newz</h3>
+                <p className="p_prot">
+                  Sky-Newz is a modern news site created with react js.{' '}
+                  <a
+                    href="https://github.com/Prabesh2004/Newssite"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <BsGithub />
+                  </a>
+                </p>
+              </div>
+            </div>
+          </a>
+        </article>
 
-   <a href='https://sky-newz.netlify.app/' target="_blank" rel="noreferrer" > <article className='portfolio_item'>
-    <div className="hold">
-      <div className="portfolio_item-image">
-        <img  src={ME} alt="Project" />
-      </div>
-      <div className="information">
-      <h3 className='h3_prot'>Sky-Newz</h3>
-      <p className='p_prot'>Sky-Newz is a modern news site created with react js.    <a href="https://github.com/Prabesh2004/Newssite" target="_blank"  rel="noopener noreferrer"><BsGithub/></a></p> 
-   
-    </div>
-    </div>
-    </article></a>
+        {/* Second */}
+        <article className="portfolio_item">
+          <a href='https://github.com/Prabesh2004/GIT-CHEAT' target="_blank" rel="noreferrer">
+            <div className="hold">
+              <div className="portfolio_item-image">
+                <img src={JS} alt="Project" />
+              </div>
+              <div className="information">
+                <h3 className="h3_prot">JS Advanced</h3>
+                <p className="p_prot">JavaScript advance topics for job interview and learning.
+                <a
+                    href="https://github.com/Prabesh2004/GIT-CHEAT"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <BsGithub />
+                  </a>
+                </p>
+              </div>
+            </div>
+          </a>
+        </article>
 
-
-{/* Sec */}
-<article className='portfolio_item'>
-<div className="hold">
-      <div className="portfolio_item-image">
-        <img  src={RE} alt="Project" />
+        {/* Third */}
+        <article className="portfolio_item">
+          <a href='/' target="_blank" rel="noreferrer">
+            <div className="hold">
+              <div className="portfolio_item-image">
+                <img src={RE} alt="Project" />
+              </div>
+              <div className="information">
+                <h3 className="h3_prot">Webdev</h3>
+                <p className="p_prot">
+                  This website is still in development. 
+                  <a
+                    href="/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <BsGithub />
+                  </a>
+                </p>
+              </div>
+            </div>
+          </a>
+        </article>
       </div>
-      <div className="information">
-      <h3 className='h3_prot'>Photoshop</h3>
-      <p className='p_prot'>Still in progress! I will update soon</p>
-      </div>
-      </div>
-    </article>
-{/* tir */}
-
-<article className='portfolio_item' target="_blank" >
-<div className="hold">
-      <div className="portfolio_item-image">
-        <img  src={RE} alt="Project" />
-      </div>
-      <div className="information">
-      <h3 className='h3_prot'>Different web-dev projects</h3>
-      <p className='p_prot'>This website is still in development. Projects will be available soon</p>
-    </div>
-</div>
-    </article>
-  </div>
-</section> )
+    </section>
+  );
 }
 
-export default Portfolio
+export default Portfolio;
